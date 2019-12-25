b18f013635c7705806eebd6fe02d2dc5bf8326dc
kevin <kevin@hinshawfam.com>
2019-12-25 08:22:18 -0600

    adds Lazily Loading Feature Modules - has bug

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,12 +6,12 @@ import { Error404Component } from './errors/404.components';
 import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventsListResolver } from './events/shared/events-list-resolver.service';
 
-export const appRoutes: Routes =[
+export const appRoutes: Routes = [
     { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent']},
     { path: 'events', component: EventsListComponent, resolve:
             { events:EventsListResolver } },
     { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator]},
     { path: '404', component: Error404Component},
-    { path: '', redirectTo: '/events', pathMatch: 'full'}
-
+    { path: '', redirectTo: '/events', pathMatch: 'full'},
+    { path: 'user', loadChildren: './user/user.module#UserModule'}
 ]
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-// import { userRoutes } from './user.routes';
-// import { ProfilComponent} from ''
+import { userRoutes } from './user.routes';
+import { ProfileComponent } from './profile.component';
 
 @NgModule({
     imports: [
-        // CommonModule,
-        // RouterModule.forChild(userRoutes)
+        CommonModule,
+        RouterModule.forChild(userRoutes)
     ],
     declarations: [
-
+        ProfileComponent
     ],
     providers: [
 
     ]
 })
-export class userModule {
+export class UserModule {
 
 }
