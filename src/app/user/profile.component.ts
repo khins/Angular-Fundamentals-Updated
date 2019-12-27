2a904cd0548d7a410f6fdcc4176a3b237572023a
khins <kevin@hinshawfam.com>
2019-12-27 13:55:53 -0600

    Revert "adds Validating Reactive Forms"
    This reverts commit 5dbbe4aba3682e328750699ded6327bb2fc1ef58.

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,34 +1,25 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { FormControl, FormGroup } from '@angular/forms'
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './profile.component.html',
-  styles: [`
-    em { float:right; color: #E056C65; padding-left: 10px; }
-    .error input { background-color:#E3C3C5;}
-    .error ::-webkit-input-placeholder { color: #999; }
-  `]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup; 
-  private firstName: FormControl;
-  private lastName: FormControl;
 
     constructor(private router:Router, private authService:AuthService){
 
     }
 
     ngOnInit() {
-      this.firstName = new FormControl
-        (this.authService.currentUser.firstName, Validators.required);
-      this.lastName = new FormControl
-        (this.authService.currentUser.lastName, Validators.required);
+      let firstName = new FormControl(this.authService.currentUser.firstName);
+      let lastName = new FormControl(this.authService.currentUser.lastName);
 
       this.profileForm =  new FormGroup({
-        firstName: this.firstName,
-        lastName: this.lastName
+        firstName: firstName,
+        lastName: lastName
       });
     }
 
@@ -37,17 +28,8 @@ export class ProfileComponent implements OnInit {
     }
 
     saveProfile(formValues) {
-      if (this.profileForm.valid) {
-        this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-        this.router.navigate(['events']);
-      }
-    }
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
+      this.router.navigate(['events']);
 
-    validateFirstName() {
-      return this.firstName.valid || this.firstName.untouched;
-     }
-
-    validateLastName() {
-     return this.lastName.valid || this.lastName.untouched;
     }
 }
