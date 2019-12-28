3576d717de17d11c123c535e0b30a91c3ea744b2
kevin <kevin@hinshawfam.com>
2019-12-28 06:12:27 -0600

    step 2 - creating reactive form - first name bug

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core'
 import { FormControl, FormGroup } from '@angular/forms'
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './profile.component.html'
 })
 export class ProfileComponent implements OnInit {
        profileForm:FormGroup;
+
+       constructor(private authService:AuthService, private router:Router){
+
+       }
   
         ngOnInit() {
-         let firstName = new FormControl();
-         let lastName = new FormControl(); 
+         let firstName = new FormControl(this.authService.currentUser.firstName);
+         let lastName = new FormControl(this.authService.currentUser.lastName); 
 
          this.profileForm = new FormGroup({
             firstName: firstName,
-            lastName: last
+            lastName: lastName
          })
+       }
+
+       cancel() {
+          this.router.navigate(['events']);
+       }
+
+       saveProfile(formValues) {
+          this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
        }
  }
