86b4ccfea51a865adedc20c72db06823c18bfe53
kevin <kevin@hinshawfam.com>
2019-12-28 06:19:47 -0600

    hitting bug Cannot read property 'firstName'

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms'
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
