3cfafe8cfc278d56e592fa734d39ae18346277a1
kevin <kevin@hinshawfam.com>
2019-12-30 07:45:16 -0600

    custom validator part 2

--- src/app/events/event-details/create-session.component.ts
+++ src/app/events/event-details/create-session.component.ts
@@ -26,7 +26,7 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required);
         this.duration = new FormControl('', Validators.required);
         this.level = new FormControl('', Validators.required);
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords]);
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])]);
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -37,11 +37,20 @@ export class CreateSessionComponent implements OnInit {
         });
     }
 
-    private restrictedWords(control:FormControl): {[key:string]:any} {
-        return control.value.includes('foo')
-            ? {'restrictedWords':'foo'}
-            : null;
+    private restrictedWords(words){
+       return (control:FormControl): {[key:string]:any} => {
+            if (!words) return null;
+
+            var invalidWords = words
+                .map(w => control.value.includes(w) ? w:null)
+                .filter(w => w != null);
+
+            return invalidWords && invalidWords.length > 0
+                ? {'restrictedWords': invalidWords.join(', ')}
+                : null;
+        }
     }
+
 
     saveSession(formValues) {
         let session:ISession ={
