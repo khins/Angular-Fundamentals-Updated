112edf49327119bcba4bc7595feaeb99a760ecb3
kevin <kevin@hinshawfam.com>
2020-01-01 08:10:44 -0600

    adds sorting data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -8,11 +8,13 @@ import { ISession } from '../shared';
 export class SessionListComponent implements OnChanges {
     @Input() sessions: ISession[];
     @Input() filterBy: string;
+    @Input() sortyBy: string;
     visibleSessions: ISession[] = [];
 
     ngOnChanges() {
         if(this.sessions) {
             this.filterSessions(this.filterBy);
+            this.sortyBy === 'name' ? this.visibleSessions.sort(sortyByNameAsc) : this.visibleSessions.sort(sortByVotesDesc);
         }
     }
 
@@ -25,4 +27,14 @@ export class SessionListComponent implements OnChanges {
             })
         }
     }
+}
+
+function sortyByNameAsc(s1: ISession, s2: ISession) {
+    if(s1.name > s2.name) return 1
+    else if(s1.name === s2.name) return 0
+    else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+  return  s2.voters.length - s1.voters.length;
 }
